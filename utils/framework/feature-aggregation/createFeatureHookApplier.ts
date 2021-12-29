51443712f5ecb130233a03aa868fcf899531ddba
fixiabis <fixiabis@gmail.com>
2021-12-29 11:36:02 +0800

    update

--- utils/framework/feature-aggregation/createFeatureHookApplier.ts
+++ utils/framework/feature-aggregation/createFeatureHookApplier.ts
@@ -1,14 +1,14 @@
-import React from 'react';
 import { extendComponent } from '../component';
+import { FeatureSource } from './type';
 
 function createFeatureHookApplier<FH extends (props: any) => any>(
   useFeature: FH,
   defaultProps?: Partial<Parameters<FH>[0]>
 ) {
-  function applyFeature<C extends React.ElementType<any>>(Component: C) {
+  function applyFeature<C extends FeatureSource>(Component: C) {
     function FeatureAppliedComponent(props: Parameters<FH>[0]) {
       const componentProps = useFeature(props);
-      return React.createElement(Component, componentProps);
+      return Component(componentProps);
     }
 
     extendComponent(FeatureAppliedComponent, Component);
